fd6cfa4377179bcb11305d83ee194cc13fcc3c3b
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-11-18 16:02:05 -0500

    first commit

--- main.js
+++ main.js
@@ -2,5 +2,15 @@ function getAttackDamage(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+let attackDamage = getAttackDamage()
+
+function currentHitPoints () {
+    return 100 - attackDamage
+}
+
+currentHit
+
 
 let currentlyAttackingMonster = boss
+
+document.getElementById("fight").addEventListener('click', getAttackDamage());
